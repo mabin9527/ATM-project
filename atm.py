87cfaf10fa879b665d144690349583657dc761ba
mabin9527 <ma.bin9527000@gmail.com>
2023-01-18 22:11:25 +0000

    Instantiate ATM class and call defined function to test passport number

--- atm.py
+++ atm.py
@@ -12,7 +12,11 @@ class ATM:
         print('Passport number should like the following example')
         print('Example: 141234567/ 151234567, G12345678, P1234567')
         passport_number = input('Please enter your passport number here: ')
-        regex = re.match('(14|15)\d{7})|(^[DEGPS]\d{7,8}$)', passport_number)
+        regex = re.match('(^(14|15)\d{7})|(^[DEGPS]\d{7,8}$)', passport_number)
         if not regex:
             print('Please enter the right passport number')
             return False
+
+
+atm = ATM()
+atm.create_account()
