467aef20e3f1961a1a7ecb791ae598e186e83002
mabin9527 <ma.bin9527000@gmail.com>
2023-01-18 22:00:36 +0000

    Add create_account function to ATM class and set up a passport number inout to allow user to enter their passport number that is verified by regular expression

---
+++ atm.py
@@ -0,0 +1,18 @@
+import re
+
+
+class ATM:
+    """
+    Collect user's personal information and generate an account number
+    """
+
+    def create_account(self):
+
+        print('--------Please enter your passport number--------')
+        print('Passport number should like the following example')
+        print('Example: 141234567/ 151234567, G12345678, P1234567')
+        passport_number = input('Please enter your passport number here: ')
+        regex = re.match('(14|15)\d{7})|(^[DEGPS]\d{7,8}$)', passport_number)
+        if not regex:
+            print('Please enter the right passport number')
+            return False
